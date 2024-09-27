9e62fe05f504416777e357237c0fc591139b1c9c
dtinth on MBP M1 <dtinth@spacet.me>
2024-09-27 14:43:08 +0700

    listen to PORT

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Elysia } from "elysia";
 const app = new Elysia()
   .use(swagger())
   .get("/", () => "Hello Elysia")
-  .listen(3000);
+  .listen(+(Bun.env["PORT"] || 46982));
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
