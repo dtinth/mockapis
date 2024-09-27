ee15304ff0679606847c306e2534038fa75e7374
dtinth on MBP M1 <dtinth@spacet.me>
2024-09-27 14:37:53 +0700

    hello

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
