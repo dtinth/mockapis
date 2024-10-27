11113bcfd2a7a65027ee929278ce76307c44f93a
Wuttinan Sukpoon <79430027+mewxz029@users.noreply.github.com>
2024-10-28 00:05:22 +0700

    Add OpenAI chat completion streaming mock api (#10)
    * Add OpenAI chat completion streaming mock api
    
    * split text to each chunks for OpenAI stream response
    
    * ensure that the official OpenAI client works with it
    
    ---------
    
    Co-authored-by: dtinth on MBP M1 <dtinth@spacet.me>

--- src/apis/openai.ts
+++ src/apis/openai.ts
@@ -37,33 +37,87 @@ const elysia = new Elysia({
   )
   .post(
     "/v1/chat/completions",
-    async ({ body }) => {
-      const { messages } = body;
+    async function* ({ body }) {
+      const { messages, stream } = body;
       const lastMessage = messages[messages.length - 1].content;
       const meowMessage = lastMessage.replace(/\w+/g, (a) =>
         a.toLowerCase() === a ? "meow" : a.toUpperCase() === a ? "MEOW" : "Meow"
       );
-      return {
+
+      if (!stream) {
+        return {
+          id: `${Date.now()}`,
+          object: "chat.completion",
+          created: Date.now(),
+          model: "meowgpt",
+          choices: [
+            {
+              index: 0,
+              message: {
+                role: "assistant",
+                content: meowMessage,
+              },
+              finish_reason: "stop",
+            },
+          ],
+          usage: {
+            prompt_tokens: lastMessage.length,
+            completion_tokens: meowMessage.length,
+            total_tokens: lastMessage.length + meowMessage.length,
+          },
+        };
+      }
+
+      const chunkMeowMessages = meowMessage.split("");
+
+      // Formats the stream according to the OpenAI API as text/event-stream
+      const toStreamMessage = (object: any) => {
+        return `data: ${JSON.stringify(object)}\n\n`;
+      };
+
+      yield toStreamMessage({
         id: `${Date.now()}`,
-        object: "chat.completion",
+        object: "chat.completion.chunk",
         created: Date.now(),
         model: "meowgpt",
+        system_fingerprint: "fp_44709d6fcb",
         choices: [
           {
             index: 0,
-            message: {
-              role: "assistant",
-              content: meowMessage,
-            },
-            finish_reason: "stop",
+            delta: { role: "assistant", content: "" },
+            logprobs: null,
+            finish_reason: null,
           },
         ],
-        usage: {
-          prompt_tokens: lastMessage.length,
-          completion_tokens: meowMessage.length,
-          total_tokens: lastMessage.length + meowMessage.length,
-        },
-      };
+      });
+      for (const meow of chunkMeowMessages) {
+        await Bun.sleep(1);
+        yield toStreamMessage({
+          id: `${Date.now()}`,
+          object: "chat.completion.chunk",
+          created: Date.now(),
+          model: "meowgpt",
+          system_fingerprint: "fp_44709d6fcb",
+          choices: [
+            {
+              index: 0,
+              delta: { content: meow },
+              logprobs: null,
+              finish_reason: null,
+            },
+          ],
+        });
+      }
+      yield toStreamMessage({
+        id: `${Date.now()}`,
+        object: "chat.completion.chunk",
+        created: Date.now(),
+        model: "meowgpt",
+        system_fingerprint: "fp_44709d6fcb",
+        choices: [
+          { index: 0, delta: {}, logprobs: null, finish_reason: "stop" },
+        ],
+      });
     },
     {
       body: t.Object({
@@ -74,28 +128,32 @@ const elysia = new Elysia({
             content: t.String(),
           })
         ),
+        stream: t.Optional(t.Boolean()),
       }),
-      response: t.Object({
-        id: t.String(),
-        object: t.String(),
-        created: t.Number(),
-        model: t.String(),
-        choices: t.Array(
-          t.Object({
-            index: t.Number(),
-            message: t.Object({
-              role: t.String(),
-              content: t.String(),
-            }),
-            finish_reason: t.String(),
-          })
-        ),
-        usage: t.Object({
-          prompt_tokens: t.Number(),
-          completion_tokens: t.Number(),
-          total_tokens: t.Number(),
+      response: t.Union([
+        t.Object({
+          id: t.String(),
+          object: t.String(),
+          created: t.Number(),
+          model: t.String(),
+          choices: t.Array(
+            t.Object({
+              index: t.Number(),
+              message: t.Object({
+                role: t.String(),
+                content: t.String(),
+              }),
+              finish_reason: t.String(),
+            })
+          ),
+          usage: t.Object({
+            prompt_tokens: t.Number(),
+            completion_tokens: t.Number(),
+            total_tokens: t.Number(),
+          }),
         }),
-      }),
+        t.AsyncIterator(t.String()),
+      ]),
       detail: { summary: "Get chat completion" },
     }
   );
