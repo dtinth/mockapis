d7bf53c47edc9884c3f4f79e5acaa746d754686f
dtinth on MBP M1 <dtinth@spacet.me>
2024-10-28 00:37:30 +0700

    review fixes

--- src/pkce.ts
+++ src/pkce.ts
@@ -1,18 +1,10 @@
 import crypto from "crypto";
 
-function base64URLEncode(buffer: Buffer) {
-  return buffer
-    .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=/g, "");
-}
-
 export function randomCodeVerifier(length: number) {
   const array = crypto.getRandomValues(new Uint8Array(length));
   return Array.from(array, (byte) => byte.toString(36))
     .join("")
-    .substr(0, length);
+    .slice(0, length);
 }
 
 export function generateCodeChallenge(codeVerifier: string, method = "plain") {
@@ -20,5 +12,5 @@ export function generateCodeChallenge(codeVerifier: string, method = "plain") {
     return codeVerifier;
   }
   const hash = crypto.createHash("sha256").update(codeVerifier).digest();
-  return base64URLEncode(hash);
+  return hash.toString("base64url");
 }
